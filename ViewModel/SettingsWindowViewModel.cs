f8dbc5daafde009c25bf5a704684ee5a9680b33b
Leo <zuosian.yang@sinopac.com>
2025-10-14 16:49:55 +0800

    feat: 增加儲存設定功能

--- ViewModel/SettingsWindowViewModel.cs
+++ ViewModel/SettingsWindowViewModel.cs
@@ -1,5 +1,10 @@
 using RadialMenu.Utility;
+using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
 using System.Windows.Input;
 
 namespace RadialMenu.ViewModel
@@ -23,7 +28,42 @@ namespace RadialMenu.ViewModel
                 OnPropertyChanged();
             }
         }
+        public bool MouseMenuEnable { get; set; }
+        public string SelectedKey { get; set; }
         public RelayCommand MouseMenuKeyDownCommand { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public ICommand CancelCommand { get; set; }
+        private void CancelCommandAction(object? obj)
+        {
+            if (obj is System.Windows.Window window)
+            {
+                window.Close();
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public ICommand SaveSettingCommand { get; set; }
+        private void SaveSettingCommandAction(object? obj)
+        {
+            var settings = new { MouseMenuEnable, SelectedKey };
+            string jsonString = JsonSerializer.Serialize(settings);
+            File.WriteAllText("settings.json", jsonString);
+
+            if (obj is System.Windows.Window window)
+            {
+                window.Close();
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
         private void MouseMenuKeyDownCommandAction(object obj)
         {
 
@@ -43,6 +83,25 @@ namespace RadialMenu.ViewModel
         {
             InitialCommand();
             InitialSelection();
+            LoadSettings();
+            SelectedKey = string.Empty;
+        }
+
+        private void LoadSettings()
+        {
+            if (File.Exists("settings.json"))
+            {
+                string jsonString = File.ReadAllText("settings.json");
+                var settings = JsonSerializer.Deserialize<JsonElement>(jsonString);
+                if (settings.TryGetProperty("MouseMenuEnable", out var mouseMenuEnableElement))
+                {
+                    MouseMenuEnable = mouseMenuEnableElement.GetBoolean();
+                }
+                if (settings.TryGetProperty("SelectedKey", out var selectedKeyElement))
+                {
+                    SelectedKey = selectedKeyElement.GetString();
+                }
+            }
         }
 
         private void InitialSelection()
@@ -66,6 +125,8 @@ namespace RadialMenu.ViewModel
         {
             MouseMenuKeyDownCommand = new RelayCommand(MouseMenuKeyDownCommandAction);
             MouseMenuKeyUpCommand = new RelayCommand(MouseMenuKeyUpCommandAction);
+            SaveSettingCommand = new RelayCommand(SaveSettingCommandAction);
+            CancelCommand = new RelayCommand(CancelCommandAction);
         }
         #endregion
     }
